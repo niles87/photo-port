8274264da5707bc770826bd10ad73d0d44bb3a3f
Niles87 <nbing19@gmail.com>
2021-03-20 11:56:28 -0700

    fixed photo importing failures, added tests to gallery and photolist components

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,20 +1,15 @@
 import React from 'react'
 import { capitalizeFirstLetter } from "../../utils/helpers";
-import photo from "../../assets/small/commercial/0.jpg";
+import PhotoList from '../PhotoList';
 
-const Gallery = (props) => {
-    const currentCatagory = {
-        name: "commercial",
-        description: "Photos of grocery stores, food trucks, and other commercial projects"
-    }
+const Gallery = ({ currentCategory }) => {
+    const { name, description } = currentCategory
 
     return (
         <section>
-            <h1>{capitalizeFirstLetter(currentCatagory.name)}</h1>
-            <p>{currentCatagory.description}</p>
-            <div className="flex-row">
-                <img src={photo} className="img-thumbnail mx-1" alt="Examples" />
-            </div>
+            <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
+            <p>{description}</p>
+            <PhotoList category={name} />
         </section>
     )
 }
