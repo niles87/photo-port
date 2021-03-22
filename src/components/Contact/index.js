d163e81bf5039d704e26806b1ad8e15f303022f4
Niles87 <nbing19@gmail.com>
2021-03-21 17:11:32 -0700

    added tests to Contact compnent, updated Nav tests

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -33,7 +33,7 @@ const ContactForm = () => {
 
     return (
         <section>
-            <h1>Contact Me</h1>
+            <h1 data-testid='h1tag'>Contact Me</h1>
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
@@ -52,7 +52,7 @@ const ContactForm = () => {
                         <p className='error-text'>{errorMessage}</p>
                     </div>
                 )}
-                <button type='submit'>Submit</button>
+                <button type='submit' data-testid='buttontag'>Submit</button>
             </form>
         </section>
     )
